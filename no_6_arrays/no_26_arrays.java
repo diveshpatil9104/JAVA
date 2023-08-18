00b984ae5541eab894d9434945a884caf5a410ec
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-08-18 23:23:17 +0530

    6th chapter

--- no_6_arrays/no_26_arrays.java
+++ no_6_arrays/no_26_arrays.java
@@ -7,7 +7,19 @@ import java.util.Scanner;
  */
 public class no_26_arrays {
 
-    public static void main(String[] args) {
+// creating function for call by reference ...  ( we will study function in next chapter)
+
+        public static void update( int marks[], int nonchangeble){       // (update) is name of function
+                nonchangeble = 10;
+                for (int i = 0; i < marks.length; i++) {
+                        marks[i] = marks[i] + 1;
+                }
+        }     
+
+
+
+
+        public static void main(String[] args) {
         /* Classroom of 500 students - You have to store marks of these 500 students You have 2 options:
            1. create 500 variables
            2. use Arrays (recommanded) 
@@ -72,10 +84,27 @@ public class no_26_arrays {
 
 // ------------------------------ PASSING ARRAY AS AN ARGUMENT ----------------------
 
-        int marks[]= {98 , 99, 100 };
+/*       there are two types for passing the variable in function (we study function or methods in next chpter ) 
+        1.  PASS BY VALUE - THEY ARE NOT REFLECT IN FUNCTION ..
+        2. PASS BY REFERENCE - when you pass an object to a FUNCTION (method) , you're passing a copy of the reference to that 
+                                object. This means changes made to the object's content within the FUNCTION affect the 
+                                original object outside the method ...
+                        [ARRAY IS TYPE OF PASS BY REFERENCE ]
         
+*/ 
 
+        int marks[] = { 98 , 99 , 100 };
+        int nonchangeble = 5;
+        update(marks , nonchangeble);
+        System.out.println(nonchangeble);
 
+        for (int i = 0; i < marks.length; i++) {
+                System.out.print(marks[i] + " ");
+                
+        }
+        System.out.println( ); // here the result is  99 , 100 , 101 bcoz we update values in function and these means changes
+                                //  made in  the FUNCTION affect the original object outside the function...
+                                //    AND nonchangable IS NOT CHANGE BCOZ OF IT IS NOT ARRAY .. 
 
 
 
