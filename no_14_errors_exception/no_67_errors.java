8cad8edaa28ffcbe74fe7132726994a8339bf6d4
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-09-28 12:07:49 +0530

    14th chapter

---
+++ no_14_errors_exception/no_67_errors.java
@@ -0,0 +1,66 @@
+package no_14_errors_exception;
+
+// import java.util.Scanner;
+
+public class no_67_errors {
+    public static void main(String[] args) {
+// ----->> SANTAX ERROR DEMO 
+/* 
+        int a = 55;
+        int b = 3             //      semicolon is missing which creates santax error 
+        System.out.println(a+b);
+   
+
+// -----> LOGICAL ERROR DEMO 
+        // WRITE A PROGRAM TO FIND FIR ST 10 PRIME NUMBERS 
+
+        System.out.println("2");
+        for (int i = 1; i < 5; i++) {
+            System.out.println(2*i+1);  // here this line print also 9, but 9  is not a prime number so,this line creats logical error
+            
+        }
+
+
+// -----> RUNTIME ERROR DEMO  
+
+        int k ;
+        Scanner sc = new Scanner(System.in); 
+        k = sc.nextInt();
+        System.out.println("Integer part of 1000 divided by k is : "  + 1000/k);
+
+        // IN ABOVE CODE WHEN USER ENTER 0 IT THROWNS RUNTIME ERROR ..WHICH IS CALLED Exception
+*/ 
+}
+    
+}
+
+
+
+
+/* 
+NO MATTER HOW SMART WE ARE ERRORS ARE OUR CONSRANT COMPANIONS. WITH PRACTICE WE ARE GETTING BETTER AT 
+    FINDING AND CORRECTING THEM.. 
+
+    - THERE ARE THREE TYPES OF ERROR IN JAVA 
+    1. SANTAX ERROR  
+    2. LOGICAL ERROR 
+    3. RUNTIME ERROR  
+
+1. SANTAX ERROR - WHEN COMPILER FINDS SOMETHING WRONG WITH OUR PROGRAM, IT THROWNS A SANTAX ERROR
+            Eg ----
+                int a = 7          ------>> semicolon is missing 
+                 System.out.println(a;
+
+
+2. LOGICAL ERROR - A LOGICAL ERROR OR BUG OCCURS WHEN A PROGRAM COMPILES AND RUN BUT DOES THE WRONG THINGS..
+
+- MASSAGE DERIVED WRONGLY 
+- WRONG TIME OF CHAT BEING DISPLAYED 
+- INCORRECT RE-DIRECTS!!
+
+3. RUNTIME ERROR - JAVA MAY SOMETIME ENCONTER WITH ERROR WHILE THE PROGRAM IS RUNNUNG THIS IS CALLED RUNTIME ERROR 
+
+---->> SANTAX AND LOGICAL ERROR ENCOUNTERED BY THE PROGRAMMER WHERE AS RUNTIME ERROR ENCOUNTERED BY USER  
+*/
+
+
