4546a5c3036901b8d2bd99bcfae8051dbbd26b31
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-10-08 00:25:25 +0530

    15th chapter

---
+++ no_15_advanced_java_1/no_83_calenderclass.java
@@ -0,0 +1,22 @@
+package no_15_advanced_java_1;
+
+import java.util.Calendar;
+
+
+public class no_83_calenderclass {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println();
+        System.out.println(calendar.getTimeZone().getID());
+        System.out.println("\n" + Calendar.getInstance());
+        System.out.println(calendar.getCalendarType());
+        System.out.println(calendar.getTime());
+
+
+            
+    
+
+
+
+    }
+}
