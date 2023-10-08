eead54f51d44c126b6110810b9243a938af9166b
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-10-09 01:52:50 +0530

    15th chapter

---
+++ no_15_advanced_java_1/no_84_gregorian.java
@@ -0,0 +1,14 @@
+package no_15_advanced_java_1;
+
+import java.util.GregorianCalendar;
+
+public class no_84_gregorian {
+    public static void main(String[] args) {
+        GregorianCalendar g = new GregorianCalendar();
+        System.out.println(g.isLeapYear(2020));
+        System.out.println(g.getTimeZone());
+        System.out.println(g.getWeeksInWeekYear());
+
+        
+    }
+}
