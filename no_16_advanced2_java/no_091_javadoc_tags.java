aa24296df0713049f2ff2528ca0a7b234fa0c4c2
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-10-12 20:34:56 +0530

    16th chapter

---
+++ no_16_advanced2_java/no_091_javadoc_tags.java
@@ -0,0 +1,25 @@
+package no_16_advanced2_java;
+
+//  TAGS FOR CLASS OR A PACKAGE 
+/**
+ * This class is demonstrate what javadoc is and how to used in the java industry
+ * @author divesh patil 
+ * @version 0.1
+ * @since 2002
+ */
+
+
+public class no_091_javadoc_tags {
+
+    static void add(int a , int b){
+        System.out.println("ADDITION IS : " + a+b);
+    }
+    public static void main(String[] args) {
+
+        System.out.println("this is main method");
+        add(4, 5 );
+
+    }
+}
+
+// created this file JavaDocs in index.html file into docs folder ... 😃    
